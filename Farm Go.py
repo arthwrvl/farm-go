a162e9e89ebe815aa1d5c4ef492d4511b4e59720
arthwrvl <arthur.vl256@gmail.com>
2022-02-08 18:02:57 -0300

    GenerateSoil

---
+++ Farm Go.py
@@ -0,0 +1,51 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+from Scripts import Soil
+
+pygame.init()
+
+#* draw soil grid on screen
+def drawGrid(cellsize, width, height):
+    soils = list()
+    gap = int(cellsize/16)
+    for i in range(4):
+        for j in range(3):
+            solo = Soil.Soil(width + i * cellsize + gap*i, height + j * cellsize + gap*j, cellsize)
+            print(solo.rect.topleft)
+            soils.append(solo)
+    return soils
+
+#!get screen size
+height = pygame.display.Info().current_h
+width = pygame.display.Info().current_w
+
+#* set background image scale
+bgimg = pygame.image.load("data/sprites/scenary/background.png")
+bgimg = pygame.transform.scale(bgimg, (width, height))
+
+#* set display
+screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+pygame.display.set_caption("Farm Go")
+
+all_sprites = pygame.sprite.Group()
+
+#* draw soil grid proportionally to screen size
+soils = drawGrid(int(width/256) * 16, int(width/7), int(height/2.5))
+all_sprites.add(soils)
+
+clock = pygame.time.Clock()
+
+#! main loop
+while True:
+    clock.tick(60)
+    screen.blit(bgimg, (0, 0))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+    all_sprites.draw(screen)
+    pygame.display.update()
+
+
---
+++ Scripts/Soil.py
@@ -0,0 +1,64 @@
+import pygame
+from pygame.locals import *
+
+
+class Soil(pygame.sprite.Sprite):
+    def __init__(self, x, y, size):
+        pygame.sprite.Sprite.__init__(self)
+
+        #*load images
+        self.dry = []
+        self.prepared = []
+        for i in range(1,4):
+            self.dry.append(pygame.image.load(f"data/sprites/scenary/Soil/Dry/{i}.png"))
+            self.prepared.append(pygame.image.load(f"data/sprites/scenary/Soil/Prepared/{i}.png"))
+
+        #*set state
+        self.state = 0 # 0 = dry, 1 = prepared, 2 = planted, 3 = need water
+
+        #*set image
+        self.image = self.dry[0]
+
+        #*set rect
+        self.x = x
+        self.y = y
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.x, self.y
+
+        #*set size
+        self.size = size
+        self.image = pygame.transform.scale(self.image, (self.size, self.size))
+
+    def ChangeState(self):
+        if self.state == 0:
+            self.image = self.dry[0]
+        elif self.state == 1:
+            self.image = self.prepared[0]
+        elif self.state == 2:
+            print("planted")
+            #add seed sprite
+        elif self.state == 3:
+            print("need water")
+            #add water sprite
+        self.update()
+
+    def interact(self):
+        if self.state == 0:
+            self.state = 1
+        elif self.state == 1:
+            self.state = 2
+        elif self.state == 3:
+            self.state = 2
+        self.ChangeState()
+
+    def update(self):
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.x, self.y
+        self.image = pygame.transform.scale(self.image, (self.size, self.size))
+    
+
+        
+
+            
+        
