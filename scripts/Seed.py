9cdca65c64bbf00d5345548115e8e2c6f7df6c86
humbertobarbosa <humbertob.tavares@gmail.com>
2022-02-09 17:35:54 -0300

    add seed sprites

--- scripts/Seed.py
+++ scripts/Seed.py
@@ -4,17 +4,26 @@ from pygame.locals import *
 
 
 class Seed:
+    seeds = [
+        pygame.image.load(f"data/sprites/itens/Seed/seed_{i}.png")
+        for i in range(len(Fruit.Fruit.fruits))
+    ]
+
     def __init__(self, growing_time):
         fruit = Fruit.Fruit.random_fruit()
         self.fruit = [fruit.name, fruit.shelf_life, fruit.img]
         self.growing_time = (growing_time + self.fruit[1]) // 2
         self.price = (self.fruit[1] - self.growing_time) * 10
-
+        self.img = self.seeds[Fruit.Fruit.fruits.index([fruit.name, fruit.shelf_life, fruit.img])]
+        
     @classmethod
     def random_growing_time(cls):
         growing_time = randint(10, 30)
 
         return cls(growing_time)
+
+    def show_img(self, screen, x, y):
+        screen.blit(self.img, (x, y))
 
     @property
     def growing_time(self):
